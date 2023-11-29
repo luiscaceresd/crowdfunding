93c6612a2ac927b2ea672f6fe6ee66d1cf9d3b57
luiscaceresd <luiscaceresd97@gmail.com>
2023-11-28 21:51:26 -0400

    adding project structure, components and pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
