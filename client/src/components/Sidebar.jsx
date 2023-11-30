8edd9a30a06de4fbc59dd2da9c5c87135d08088f
luiscaceresd <luiscaceresd97@gmail.com>
2023-11-29 21:59:11 -0400

    adding searchbar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -22,7 +22,6 @@ const Sidebar = () => {
   const navigate = useNavigate()
   const [isActive, setIsActive] = useState('dashboard')
 
-
   return (
     <div className='flex justify-between items-center flex-col sticky top-5 h-[93vh]'>
       <Link to="/">
